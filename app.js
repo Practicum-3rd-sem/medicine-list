e0e8be35025341cb1c6ab69b720471cc43287394
Gunjan Bhanarkar <82956298+gunjan1909@users.noreply.github.com>
2021-10-11 12:41:13 +0530

    initial upload

---
+++ app.js
@@ -0,0 +1,28 @@
+let myform = document.getElementById("myform");
+let medname = document.getElementById("medname");
+let timing = document.getElementById("timing");
+let frequency = document.getElementById("frequency");
+let tasklist = document.getElementById("tasklist");
+myform.addEventListener("submit", (x) => {
+  x.preventDefault(); //prevent default action of browser after submitting ie refresh/submit form
+  createitem(medname.value, timing.value, frequency.value);
+});
+
+function createitem(x, y, z) {
+  /*let myhtml = `<li>${z} <button onclick="itemdeleter(this)">Done</button></li>`;*/
+  let myhtml = `<tr> 
+                <td><i class="fas fa-prescription-bottle-alt"></i>${x}</td>
+                <td>${y}</td>
+                <td>${z}</td>
+                <td><button onclick="itemdeleter(this)">Done</button></td>
+                </tr>`;
+  tasklist.insertAdjacentHTML("beforeend", myhtml);
+  medname.value = "";
+  timing.value = "";
+  frequency.value = "";
+  medname.focus();
+}
+
+function itemdeleter(y) {
+  y.parentElement.parentElement.remove();
+}
